dc7f5b3ba2ca6ef83c6e1394f5954de5f10f3cd8
Hakan Kemal Çuhadar <37040780+hakan0814@users.noreply.github.com>
2019-09-25 11:35:45 +0200

    Add files via upload

---
+++ my-personal-page/scripts/main.js
@@ -0,0 +1,16 @@
+document.getElementByID('index').style.backgroundImage="images/IMG_7293.jpg";
+
+let d = new Date();
+alert("Just to let you know, today's date is " + d);
+
+// $(document).ready(function(){
+//
+//     // Set background image of a div on click of the button
+//
+//     $("button").click(function(){
+//         var imageUrl = "images/IMG_7293.jpg";
+//         $("#index").css("background-image", "url(" + imageUrl + ")");
+//
+//     });
+//
+// });
